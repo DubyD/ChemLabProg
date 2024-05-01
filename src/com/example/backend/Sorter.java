24209ad3c42d35bc5c0f8588a565eebd3ccc3669
DubyD <140568071+DubyD@users.noreply.github.com>
2024-05-01 12:29:33 -0400

    notes

--- src/com/example/backend/Sorter.java
+++ src/com/example/backend/Sorter.java
@@ -10,4 +10,33 @@ public static class Sorter(){
         //no constructor because you cannot creat a static class
 
 
+
+            //These three are the same format and can use the same function
+            //We need to separate column 10 into it's separate hazards. in order to
+            //extract flammability and corrosiveness
+//----------------------------------------------------------------------------------------------------------------------
+        //For 534_inventory, the columns are set up as so: (skip line 1)
+        //SDS,Chemical,Company,Room,Location,Amount of Jars,Amount,Unit,CAS #s,Hazard
+        //We need columns 2, 3(so the professor can determine if they enjoy the product)
+        //4,5,6, 7 and 8 can be combined, and 10
+
+        //For W412C_inventory the columns are set up as so: (skip line 1)
+        //blank,Chemical,Company,Room,Location,Amount of Jars,Amount,Unit,CAS #s,Hazard
+        //We need column 2,3(so the professor can determine if they enjoy the product)
+        //4,5,6, 7 and 8 can be combined, and 10
+
+        //For W412D_inventory the columns are set up as so: (skip line 1)
+        //blank,Chemical,Company,Room,Shelf,Amount of Jars/Containers,Amount,Unit,CAS #s,Hazard
+        //We need column 2,3(so the professor can determine if they enjoy the product)
+        //4,5,6, 7 and 8 can be combined, and 10
+//----------------------------------------------------------------------------------------------------------------------
+
+        //For new_solutions_Cab (for cabinet) the columns are set up as so: (skip line 1)
+        //Name,Quantity,Initials/Date ,Location,Company
+        //These are custom chems so may need a subclass of Chemical to store data properly
+        //We need columns 1,2,3,4
+
+
+
+
 }
