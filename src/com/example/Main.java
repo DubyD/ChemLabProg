748138f8d406b1be0211cfdf40bc54c476c8a66c
SalimJday <144251605+SalimJday@users.noreply.github.com>
2024-04-30 23:29:50 -0400

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -27,3 +27,42 @@ public class Main {
         //setup the screen
     }
 }
+
+
+/*
+
+//Some other version of code, mostly for testing. By SJ
+
+package com.example;
+
+import com.example.frontend.SceneSwitcher;
+
+import frontend.SceneSwitcher;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a new JFrame
+        JFrame frame = new JFrame("Science Department Inventory");
+
+        // Set the default close operation
+        frame.setSize(600, 400);
+
+        // Set the size of the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        new SceneSwitcher(frame);
+
+        // Set the visibility of the frame
+        frame.setVisible(true);
+
+        // TODO make and trigger the login here
+
+        // post login
+
+        //setup the screen
+    }
+}
+
+*/
