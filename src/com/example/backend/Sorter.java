4886a1cbde5f8c6c71ab4ad56fbe92cf90b814ad
DubyD <140568071+DubyD@users.noreply.github.com>
2024-05-01 11:03:26 -0400

    adding files

--- src/com/example/backend/Sorter.java
+++ src/com/example/backend/Sorter.java
@@ -1,12 +1,13 @@
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-    //This class will be used to read and write data entries to the .csv file
-    //
+    // This class will be used to read and write data entries to the updated_data.csv file
+    // All functions related to retrieving or
 public static class Sorter(){
 
 
-        //St
+        //no constructor because you cannot creat a static class
 
 
 }
