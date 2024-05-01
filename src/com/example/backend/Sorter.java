bca65d0411ae47b7a1f86cedf06c803db34a7718
DubyD <140568071+DubyD@users.noreply.github.com>
2024-05-01 12:42:43 -0400

    cleaned up data

--- src/com/example/backend/Sorter.java
+++ src/com/example/backend/Sorter.java
@@ -12,8 +12,7 @@ public static class Sorter(){
 
 
             //These three are the same format and can use the same function
-            //We need to separate column 10 into it's separate hazards. in order to
-            //extract flammability and corrosiveness
+            //We need to search through column 10 in order to extract flammability and corrosiveness
 //----------------------------------------------------------------------------------------------------------------------
         //For 534_inventory, the columns are set up as so: (skip line 1)
         //SDS,Chemical,Company,Room,Location,Amount of Jars,Amount,Unit,CAS #s,Hazard
