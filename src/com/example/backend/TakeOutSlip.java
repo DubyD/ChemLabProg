98864755521103a6ee226d05a805251d67f58b8f
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-05-02 22:57:26 -0400

    started with takeoutslip basics

--- src/com/example/backend/TakeOutSlip.java
+++ src/com/example/backend/TakeOutSlip.java
@@ -1,4 +1,27 @@
 package com.example.backend;
 
+/**
+ * TakeOutSlip
+ * @author Ahmed Khan
+ * First draft of a class designed to register and update
+ * the amount of chemicals taken from containers
+ * and drafting up a slip of which user was responsible 
+ * for which action.
+ */
 public class TakeOutSlip {
+    private User user; // spaceholder for the details of the user making changes to chemicals
+    private Chemical chemical // spaceholder for the chemical object to be altered
+    public TakeOutSlip(User userDet, Chemical chemicalInput){
+        this.user = userDet;
+        this.chemical=chemicalInput;
+
+        // need to setup method to alter the chemicals based on specification
+    }
+
+    public String takeOutDetails(){
+        String details = "User: " + this.user.getUsername + "altered chemical" + this.chemical.getName(); + "by this amount" + "5mL";
+        return details;
+        // 5mL is spaceholder amount, will need to make it compare chemical sizes before and after alteration.
+        // and return that value - AK
+    }
 }
