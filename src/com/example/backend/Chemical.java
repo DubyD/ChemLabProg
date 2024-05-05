0e0d3977415c281cc45e9894483e529315b5300f
Alex <17982529+alexm622@users.noreply.github.com>
2024-05-05 14:19:29 -0400

    fixed this dumpster fire of a class

--- src/com/example/backend/Chemical.java
+++ src/com/example/backend/Chemical.java
@@ -2,7 +2,8 @@
 
 package com.example.backend;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Chemical {
 
@@ -11,22 +12,18 @@ public class Chemical {
     private String manufacturer;
     private String room;
     private String shelf;
-    private containers;
+    private int containers;
     private double size;
     private String sizeUnit;
-    private casNum;
+    private String casNum;
     private String hazards;
-    private boolean flammable;
-    private boolean corrosive;
-    private boolean skinIrritant;
-    private boolean eyeIrritant;
-    private boolean oralHazard;
-    private boolean combustible;
-    private boolean aquaticHazard;
-    private boolean respitoryHazard;
+    private boolean flammable = false;
+    private HashMap<String, Boolean> hazardFlags = new HashMap<String, Boolean>();
 
     private String purchaseDate;
     private String expirationDate;
+
+    final String[] UNITS = {"ml", "g","oz","lb","mg","l","kg","gal","qt","pt","cup","tbsp","tsp","fl oz","mL","L","g","mg","kg","oz","lb","gal","qt","pt","cup","tbsp","tsp","fl oz"};
 
     //Auto Constructor used in sorter class
     public Chemical(String name, String company, String room, String shelf, int containers, Double amount, String unit, String cas, String hazards){
@@ -40,78 +37,59 @@ public class Chemical {
         this.sizeUnit = unit;
         this.casNum = cas;
 
-        this.corrosive = false;
-        this.skinIrritant = false;
-        this.eyeIrritant = false;
-        this.oralHazard = false;
-        this.combustible = false;
-        this.aquaticHazard = false;
-        this.respitoryHazard = false;
-
         this.hazards = hazards;
 
 
 
 
         //Sets specific Hazards
-        this.setHazards(hazards);
+        this.setHazardFlags(hazards);
 
-        validateSize(size);
-        validateSizeUnit(sizeUnit);
+        if(size < 0){
+            throw new IllegalArgumentException("Size must be a positive number.");
+        }
+        if(!Arrays.asList(UNITS).contains(sizeUnit) || sizeUnit == null){
+            //default to g
+            sizeUnit = "g";
+        }
     }
 
-    private void setHazards(String list){
+    private void setHazardFlags(String list){
         String[] pieces = list.split(",");
         for(String next : pieces){
             String[] parts = next.split(" ");
-            for(String bits : parts){
-                if(bits.equals("eye")){
-                    this.eyeIrritant = true;
+            for(String hazard : parts){
+                if(hazard.equals("eye")){
+                    hazardFlags.put("eyeIrritant", true);
                 }
-                if(bits.equals("skin")){
-                    this.skinIrritant = true;
+                if(hazard.equals("skin")){
+                    hazardFlags.put("skinIrritant", true);
                 }
-                if(bits.equals("corrosive")){
-                    this.corrosive = true;
+                if(hazard.equals("corrosive")){
+                    hazardFlags.put("corrosive", true);
                 }
-                if(bits.equals("combustible")){
-                    this.combustible = true;
+                if(hazard.equals("combustible")){
+                    hazardFlags.put("combustible", true);
                 }
-                if(bits.equals("oral") || bits.equals("digestive")){
-                    this.oralHazard = true;
+                if(hazard.equals("oral") || hazard.equals("digestive")){
+                    hazardFlags.put("oralHazard", true);
                 }
-                if(bits.equals("flammable")){
-                    this.flammable = true;
+                if(hazard.equals("flammable")){
+                    hazardFlags.put("flammable", true);
                 }
-                if(bits.equals("aquatic")){
-                    this.aquaticHazard = true;
+                if(hazard.equals("aquatic")){
+                    hazardFlags.put("aquaticHazard", true);
                 }
-                if(bits.equals("respitory")){
-                    this.respitoryHazard = true;
+                if(hazard.equals("respiratory")){
+                    hazardFlags.put("respiratoryHazard", true);
                 }
             }
         }
     }
 
-    // Getters and setters for all attributes
-
-
-    public List<Room> getRoomsStoredAt() {
-        return roomsStoredAt;
+    public HashMap<String, Boolean> getHazardFlags(){
+        return hazardFlags;
     }
-
-    public void addRoomStoredAt(Room room) {
-        roomsStoredAt.add(room);
-    }
-
-    public List<Shelf> getShelvesStoredAt() {
-        return shelvesStoredAt;
-    }
-
-    public void addShelfStoredAt(Shelf shelf) {
-        shelvesStoredAt.add(shelf);
-    }
-
 
     public String getManufacturer() {
         return manufacturer;
@@ -185,13 +163,8 @@ public class Chemical {
         this.sdsSheet = sdsSheet;
     }
 
-    // Validation methods, We can add more, this is just first draft
-    private boolean isPositiveSize(double size) {
-        return size > 0;
-    }
-
     public void updateSize(double newSize) {
-        if (isPositiveSize(newSize)) {
+        if (size > 0) {
             this.size = newSize;
         } else {
             // Handle invalid size
@@ -199,10 +172,49 @@ public class Chemical {
         }
     }
 
-    // Validation method for sizeUnit
-    private boolean isValidSizeUnit(String sizeUnit) {
-        return sizeUnit.equalsIgnoreCase("ml") || sizeUnit.equalsIgnoreCase("gm");
+
+    public String getRoom() {
+        return this.room;
     }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getShelf() {
+        return this.shelf;
+    }
+
+    public void setShelf(String shelf) {
+        this.shelf = shelf;
+    }
+
+    public int getContainers() {
+        return this.containers;
+    }
+
+    public void setContainers(int containers) {
+        this.containers = containers;
+    }
+
+    public String getCasNum() {
+        return this.casNum;
+    }
+
+    public void setCasNum(String casNum) {
+        this.casNum = casNum;
+    }
+
+    public boolean getFlammable() {
+        return this.flammable;
+    }
+
+
+    public String[] getUNITS() {
+        return this.UNITS;
+    }
+
+
 
     // needed for SearchResultsPanel
     // made by Alex Comeau
@@ -211,8 +223,8 @@ public class Chemical {
     }
 
     public String[] asArray() {
-        return new String[] { hasSDS() ? "Yes" : "No", name, manufacturer, roomsStoredAt.toString(),
-                shelvesStoredAt.toString(), "Amount of Jars", String.valueOf(size), sizeUnit, "CAS #s", hazards };
+        return new String[] { hasSDS() ? "Yes" : "No", name, manufacturer, room,
+                shelf, "Amount of Jars", String.valueOf(size), sizeUnit, "CAS #s", hazards };
 
     }
 
