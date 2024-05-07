b52ad3d95f5b02c5febb9e51c49af10de2f915cd
guswarmington <guswarmington@gmail.com>
2024-05-07 12:51:46 -0400

    updated adminScene
    just some functionality updates, will make it prettier soon

--- src/com/example/frontend/AdminScene.java
+++ src/com/example/frontend/AdminScene.java
@@ -60,101 +60,190 @@ public class AdminScene extends JPanel{
     }
 
     private void runScene() {
-        unapprovedUsers.add(unapprovedUsers.getSize(), new User("Gus", "password"));
-        // Temporary test user
-
+         unapprovedUsers.add(unapprovedUsers.getSize(), new User("Gus", "password"));
+        //Temporary test user
         JButton approveUserButton = new JButton("Approve");
         approveUserButton.setVisible(false);
         JButton denyUserButton = new JButton("Deny");
         denyUserButton.setVisible(false);
         
-        cUserList = new JList<>(approvedUsers);
-        cUserList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        pUserList = new JList<>(unapprovedUsers);
-        pUserList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        JButton removeUserButton = new JButton("Remove");
+        removeUserButton.setVisible(false);
+        JButton makeAdminButton = new JButton("Set Admin");
+        makeAdminButton.setVisible(false);
+        
+        JButton currentUsersButton = new JButton("Active Users (" + approvedUsers.getSize() +")" );
+        currentUsersButton.addActionListener(event ->{
+        if(cListScroller.isVisible()){
+            cListScroller.setVisible(false);
+            removeUserButton.setVisible(false);
+            makeAdminButton.setVisible(false);
+        }
+        else{
+            cListScroller.setVisible(true);
+            removeUserButton.setVisible(true);
+            makeAdminButton.setVisible(true);
+        }
+            pListScroller.setVisible(false);
+            approveUserButton.setVisible(false);
+            denyUserButton.setVisible(false);
+            
+            frame.revalidate();
+        });
+        
+        JButton pendingUsersButton = new JButton("Pending Users (" + unapprovedUsers.getSize() +")" );
+        pendingUsersButton.addActionListener(event ->{
+        if(pListScroller.isVisible()){
+            pListScroller.setVisible(false);
+            approveUserButton.setVisible(false);
+            denyUserButton.setVisible(false);
 
-        cListScroller = new JScrollPane(cUserList);
+        }
+        else{
+            pListScroller.setVisible(true);
+            approveUserButton.setVisible(true);
+            denyUserButton.setVisible(true);
+        }   
+            removeUserButton.setVisible(false);
+            makeAdminButton.setVisible(false);
+            cListScroller.setVisible(false);
+            frame.revalidate();
+        });
+
+        //Current User List {
+        cUserList = new JList(approvedUsers);
+        cUserList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        
+       cListScroller = new JScrollPane(cUserList);
         cListScroller.setWheelScrollingEnabled(true);
         cListScroller.createVerticalScrollBar();
         cListScroller.setPreferredSize(new Dimension(200, 70));
         cListScroller.setVisible(false);
-
+        //}
+        //Pending User List {
+        pUserList = new JList(unapprovedUsers);
+        pUserList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        
         pListScroller = new JScrollPane(pUserList);
         pListScroller.setWheelScrollingEnabled(true);
         pListScroller.createVerticalScrollBar();
         pListScroller.setPreferredSize(new Dimension(200, 70));
         pListScroller.setVisible(false);
-        /*
-         * pUserList.addListSelectionListener(event ->{
-         * approveUserButton.setVisible(true);
-         * denyUserButton.setVisible(true);
-         * });
-         */
+        //}
+        
 
-        JButton currentUsersButton = new JButton("Active Users (" + approvedUsers.size() + ")");
-        currentUsersButton.addActionListener(event -> {
-            if (cListScroller.isVisible()) {
-                cListScroller.setVisible(false);
-            } else {
-                cListScroller.setVisible(true);
+        
+        approveUserButton.addActionListener(event ->{
+            if(pUserList.isVisible()){
+                for(int i: pUserList.getSelectedIndices()){
+                    if(i >= 0 ){
+                    saveNewUser((User) unapprovedUsers.get(i));
+                    }
+                }
+                pUserList.clearSelection();
+                pendingUsersButton.setText("Pending Users (" + unapprovedUsers.getSize() +")" );
+                currentUsersButton.setText("Active Users (" + approvedUsers.getSize() +")" );
+                pendingUsersButton.repaint();
+                this.revalidate();
+                this.repaint();
             }
-            pListScroller.setVisible(false);
-            approveUserButton.setVisible(false);
-            denyUserButton.setVisible(false);
+        });
+        denyUserButton.addActionListener(event ->{
+            if(pUserList.isVisible()){
+                for(int i: pUserList.getSelectedIndices()){
+                    if(i >= 0 && unapprovedUsers.getSize() > 0){
+                    unapprovedUsers.remove(i);
+                    pUserList.remove(i);
+                    }
+                }
 
-            frame.revalidate();
+                pUserList.clearSelection();
+                pendingUsersButton.setText("Pending Users (" + unapprovedUsers.getSize() +")" );
+                
+                pendingUsersButton.repaint();
+                this.revalidate();
+                this.repaint();
+            }
         });
 
-        JButton pendingUsersButton = new JButton("Pending Users (" + unapprovedUsers.size() + ")");
-        pendingUsersButton.addActionListener(event -> {
-            if (pListScroller.isVisible()) {
-                pListScroller.setVisible(false);
-                approveUserButton.setVisible(false);
-                denyUserButton.setVisible(false);
-            } else {
-                pListScroller.setVisible(true);
-                approveUserButton.setVisible(true);
-                denyUserButton.setVisible(true);
-            }
-            cListScroller.setVisible(false);
-            frame.revalidate();
+        
+        removeUserButton.addActionListener(event ->{
+           for(int i: cUserList.getSelectedIndices()){
+                    if(i >= 0){
+                    approvedUsers.remove(i);
+                    cUserList.remove(i);
+                    }
+                }
+
+                cUserList.clearSelection();
+                currentUsersButton.setText("Active Users (" + approvedUsers.getSize() +")" );
+                currentUsersButton.repaint();
+                this.revalidate();
+                this.repaint();
         });
 
+        makeAdminButton.addActionListener(event ->{
+            if(cUserList.isVisible()){
+                for(int i: cUserList.getSelectedIndices()){
+                    if(i >= 0){
+                    makeAdmin(approvedUsers.get(i));
+                    }
+                }
+
+                this.revalidate();
+                this.repaint();
+                
+            }
+        });
+        
+        
+        
+        
+
+        /* pUserList.addListSelectionListener(event ->{
+        approveUserButton.setVisible(true);
+        denyUserButton.setVisible(true);
+        });*/
+        
+
+        
         this.add(currentUsersButton);
         this.add(pendingUsersButton);
         this.add(cListScroller);
         this.add(pListScroller);
         this.add(approveUserButton);
         this.add(denyUserButton);
-
-        returnButton = new JButton("Return");
-
-        this.add(returnButton);
+        this.add(makeAdminButton);
+        this.add(removeUserButton);
+        
+        /*        returnButton = new JButton("Return");
+        
+        this.add(returnButton);*/
         frame.add(this);
         frame.revalidate();
+    
     }
 
-    public void createNewUser(String username, String password) {
-        // Not implemented yet
+    public void createNewUser(String username, String password){
         User user = new User(username, password);
-        unapprovedUsers.add(unapprovedUsers.getSize(), user);
-
+        unapprovedUsers.addElement(user);
+        
     }
-
-    {
-
-    }
-
-    private void saveNewUser(User user) {
-        // Not implemented yet
-        approvedUsers.add(unapprovedUsers.getSize(), user);
+    
+    private void saveNewUser(User user){
+        //Not implemented yet
+        approvedUsers.addElement(user);
         unapprovedUsers.removeElement(user);
 
     }
-
-    public JButton getReturnButton() {
-        return returnButton;
+    
+    private void makeAdmin(User user){
+        user.setAdmin(true);
     }
+
+    /*    public JButton getReturnButton() {
+    return returnButton;
+    }*/
 
     public String toString() {
         return "This class holds a user management screen.";
